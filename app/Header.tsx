12dee1dc90f4302e83678a014f0bf8bab91d13f5
xsantanax <rafaelsantana111@gmail.com>
2023-02-07 15:46:43 -0300

    layout

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,6 @@ import Link from "next/link"
 function Header() {
   return (
     <header className="p-5 bg-blue-500">
-      <p className="font-bold text-white">I am the Header</p>
       <Link href="/" className="px-2 py-1 bg-white text-blue-500 rounded-lg">
         Home
       </Link>
