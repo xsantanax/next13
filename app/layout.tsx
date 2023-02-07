0bcd7725b0857933bd6be7bcd34ff86076bd6ce9
xsantanax <rafaelsantana111@gmail.com>
2023-02-07 15:43:40 -0300

    header + todo page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
+import "../styles/globals.css"
+import Header from "./Header"
+
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 function Home() {
-  return <div>This is Home</div>
+  return <div className="text-red-500">This is Home</div>
 }
 
 export default Home
