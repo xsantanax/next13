093283d33b9bbf9519152f4a15cc0ceba1d453b1
xsantanax <rafaelsantana111@gmail.com>
2023-02-07 22:42:20 -0300

    fetchTodo (by id)

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Todo } from "../../../typings"
 
 type PageProps = {
   params: {
@@ -6,8 +7,28 @@ type PageProps = {
   }
 }
 
-function TodoPage({ params: { todoId } }: PageProps) {
-  return <div>TodoPage: {todoId}</div>
+const fetchTodo = async (todoId: string) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`
+  )
+  const todo = await response.json()
+  return todo
+}
+
+async function TodoPage({ params: { todoId } }: PageProps) {
+  const todo: Todo = await fetchTodo(todoId)
+  //   return <div>TodoPage: {todoId}</div>
+  return (
+    <div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg">
+      <p>
+        #{todo.id}:{todo.title}
+      </p>
+      <p>Completed: {todo.completed ? "Yes" : "No"}</p>
+      <p className="border-t border-black mt-5 text-right">
+        By User: {todo.userId}
+      </p>
+    </div>
+  )
 }
 
 export default TodoPage
